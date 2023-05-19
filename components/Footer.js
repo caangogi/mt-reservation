4e064840a0fc2693ad9214a6ff597d80e247e8c2
andresc4f <99268322+caangogi@users.noreply.github.com>
2023-05-19 23:27:50 +0200

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,40 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+    <div className='flex justify-center items-center gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
       <div className='space-y-4 text-xs text-gray-800'>
         <h5 className='font-bold'>ABOUT</h5>
-        <p>How Cwod4Flippin works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>C4F Plus</p>
-        <p>c4F Luxe</p>
+        <p>Como trabajamos en Mallorca Transfer</p>
+        <p>Nuestros vehículos</p>
+        <p>Mallorca Transfer Plus</p>
+        <p>Mallorca Transfer Luxe</p>
       </div>
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>COMMUNITY</h5>
-        <p>How Cwod4Flippin works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>C4F Plus</p>
-        <p>c4F Luxe</p>
-      </div>
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>HOST</h5>
-        <p>How Cwod4Flippin works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>C4F Plus</p>
-        <p>c4F Luxe</p>
-      </div>
-      <div className='space-y-4 text-xs text-gray-800'>
-        <h5 className='font-bold'>SUPPORT</h5>
-        <p>How Cwod4Flippin works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>C4F Plus</p>
-        <p>c4F Luxe</p>
-      </div>
-      
     </div>
   )
 }
