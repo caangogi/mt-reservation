7017bae38b145f0f2365a9800aa7186331253cfc
caangogi <99268322+caangogi@users.noreply.github.com>
2025-02-06 14:45:35 +0100

    Update Create.ts

--- backend/user/aplication/Create.ts
+++ backend/user/aplication/Create.ts
@@ -1,10 +1,12 @@
+import { doc, setDoc, collection } from "firebase/firestore";
 import { db } from "../../../services/FirebaseService";
 import { User } from "../../share/types";
 
 export class CreateUser {
   async create(user: User): Promise<void> {
     try {
-      await db.collection('users').doc(user.uid).set({
+      const userRef = doc(collection(db, "users"), user.uid);
+      await setDoc(userRef, {
         name: user.name,
         lastName: user.lastName,
         documentType: user.documentType,
@@ -12,7 +14,6 @@ export class CreateUser {
         phone: user.phone,
         type: user.type,
       });
-
       console.log("Usuario creado exitosamente en Firestore");
     } catch (error) {
       console.error("Error al crear el usuario en Firestore:", error);
